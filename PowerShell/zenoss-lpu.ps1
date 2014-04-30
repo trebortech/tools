3badc54c26f0ef92b9d8f5c99d47a47bd069de7f
Robert Booth <rbooth@zenoss.com>
2014-04-30 10:37:20 -0500

    Working ps script for NameSpace security modification.

--- PowerShell/zenoss-lpu.ps1
+++ PowerShell/zenoss-lpu.ps1
@@ -20,14 +20,28 @@
 #>
 
 
-$username
-$userfqdn			# Example zenmonitor@zenoss.com
-
+$username = 'zenny'					# Username alone
+$domaintype = 'domain'		# local or domain
 
 # The following values will be set at runtime. They are place holders here.
 $usersid
-$userdomain
 
+# Default settings
+$inherit = $True      # Set to false (not recommended) if you do not want WMI Acl inheritance
+$namespaceParams = @{Namespace=$namespace;Path="__systemsecurity=@"}
+
+$OBJECT_INHERIT_ACE_FLAG = 0x1
+$CONTAINER_INHERIT_ACE_FLAG = 0x2
+
+# Set account information
+if($domaintype.ToLower() -ne 'local'){
+	$domain = $env:USERDOMAIN
+	$userfqdn = "{0}@{1}" -f $username, $domain 
+}
+else {
+	$domain = $env:USERDNSDOMAIN
+	$userfqdn = "{1}\{0}" -f $username, $domain
+}
 
 
 function get_user_sid($getuser=$userfqdn) {
@@ -36,27 +50,104 @@ function get_user_sid($getuser=$userfqdn) {
 	return $objSID.Value
 }
 
-
-
-function allow_access_to_winrm($username) {
+function allow_access_to_winrm($usersid) {
 	#check to confirm sid is set.
-	if $usersid.Length -gt 5 {
+	if($usersid.Length -gt 5) {
 		$sddlstart = (Get-Item WSMan:\localhost\Service\RootSDDL).Value
-
 	} 
 	else {
-	    #send error message
+		throw "Error getting WinRM SDDL"
 	}
-	$sddlstart = ()
 }
 
-function allow_access_to_wminamespace($ns, $user=){
+function get_accessmask($permissions){
+	<#
+	$permission = Enable,MethodExecute,ReadSecurity,RemoteAccess
+	#>
 
-	
+	$permTable = @{
+		"enable" 			= 1;
+		"methodexecute" 	= 2;
+		"fullwrite"			= 4;
+		"partialwrite"		= 8;
+		"providerwrite"		= 0x10;
+		"remoteaccess"		= 0x20;
+		"readsecurity"		= 0x20000;
+		"writesecurity"		= 0x40000
+	}
+
+	$accessMask = 0
+	foreach ($perm in $permissions) {
+		$perm = $perm.ToLower()
+		if($permTable.ContainsKey($perm)){
+			$accessMask += $permTable[$perm]
+		}
+		else {
+		    throw "Unknown permission: $perm"
+		}
+	}
+	return $accessMask
 }
 
+<#
+
+Might be able to remove this function
+
+function get_user_account(){
+	$accountparams = @{
+		Class="Win32_Account";
+		Filter="Domain='$domain' and Name='$username'"}
+
+	$objUser = Get-WmiObject @accountparams
+
+	if ($objUser -eq $null){
+		throw "Account was not found: $userfqdn"
+	}
+
+	return $objUser
+}
+#>
+
+function add_ace($accessMask, $namespace){
+	$currentSecurityDescriptor = Invoke-WmiMethod @namespaceParams -Name GetSecurityDescriptor
+	if($currentSecurityDescriptor.ReturnValue -ne 0){
+		throw "Failed to get security descriptor for namespace: $namespace"
+	}
+	$objACL = $currentSecurityDescriptor.Descriptor
+
+	$objACE = (New-Object System.Management.ManagementClass("win32_Ace")).CreateInstance()
+	$objACE.AccessMask = $accessMask
+	if ($inherit){
+		$objACE.AceFlags = $CONTAINER_INHERIT_ACE_FLAG
+	}
+	else {
+	    $objACE.AceFlags = 0
+	}
+	$objTrust = (New-Object System.Management.ManagementClass("win32_Trustee")).CreateInstance()
+	$objTrust.SidString = $usersid
+	$objACE.Trustee = $objTrust
+	$objACE.AceType = 0x0
+	$objACL.DACL += $objACE.psobject.immediateBaseObject
+	$daclparams = @{
+		Name="SetSecurityDescriptor";
+		ArgumentList=$objACL.psobject.immediateBaseObject
+	} + $namespaceParams
+	$setresults = Invoke-WmiMethod @daclparams
+	if ($setresults.ReturnValue -ne 0) {
+		throw "Set Security Descriptor FAILED: $($setresults.ReturnValue)"
+		}
+}
+
+function allow_access_to_wminamespace($namespace){
+	$permissions = @("Enable","MethodExecute","ReadSecurity","RemoteAccess")
+	$accessmap = get_accessmask $permissions
+	add_ace $accessmap $namespace
+}
 
 
 # Initialize user information
 $usersid = get_user_sid
-
+$namespaces = @("Root", "root/CIMv2")
+foreach ($namespace in $namespaces) {
+	allow_access_to_wminamespace($namespace)
+}
